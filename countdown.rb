43c70d88d96049c1ffb107afaad33c02140a8c2e
Philip Levy <philipdlevy@gmail.com>
2022-05-21 10:32:00 -0600

    commit tree 311395ced232196b85a6d3287d61104340103192

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(number)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+    number = 10
+    while number > 0
+      puts "#{number} SECOND(S)!"
+      number -= 1
+      sleep(1.0)
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
